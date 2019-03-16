1ed2feb238537252dae6378a1b404653dabe2458
johsire <josiah5115@gmail.com>
2019-03-15 18:45:46 -0600

    Set-State & Fetch Customer Names

--- client/src/App.js
+++ client/src/App.js
@@ -9,9 +9,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Customers />
         </header>
-        {/* <Customers /> */}
+        <Customers />
       </div>
     );
   }
